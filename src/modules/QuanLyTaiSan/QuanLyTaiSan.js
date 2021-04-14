832e474888b82610db8bc545f86c3a4a2291c010
ntduyenptit <nguyenthiduyen.ptit@gmail.com>
2021-04-14 16:32:13 +0700

    navigative

---
+++ src/modules/QuanLyTaiSan/QuanLyTaiSan.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  ImageBackground,
+} from 'react-native';
+
+import { fonts, colors } from '../../styles';
+import { Text } from '../../components/StyledText';
+export default function QuanLyTaiSan({ isExtended, setIsExtended }) {
+  return (
+    <View style={styles.container}>
+     
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  bgImage: {
+    flex: 1,
+    marginHorizontal: -20,
+  },
+  section: {
+    flex: 1,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  sectionLarge: {
+    flex: 2,
+    justifyContent: 'space-around',
+  },
+  sectionHeader: {
+    marginBottom: 8,
+  },
+
+  description: {
+    padding: 15,
+    lineHeight: 25,
+  },
+  titleDescription: {
+    color: '#19e7f7',
+    textAlign: 'center',
+    fontFamily: fonts.primaryRegular,
+    fontSize: 15,
+  },
+  title: {
+    marginTop: 30,
+  },
+  price: {
+    marginBottom: 5,
+  },
+  textStyle: {
+    color: 'black',
+    fontSize: 15,
+  }
+});
