646c3a0e2594c340885c2b3ef61c159f271e9165
ntduyenptit <nguyenthiduyen.ptit@gmail.com>
2021-05-28 17:31:00 +0700

    them moi vai tro

--- src/modules/quanlyhethong/quanlyphanquyen/QuanLyPhanQuyen.js
+++ src/modules/quanlyhethong/quanlyphanquyen/QuanLyPhanQuyen.js
@@ -1,12 +1,12 @@
 /* eslint-disable import/no-cycle */
 import React from 'react';
-import { Animated, SafeAreaView, StatusBar, Dimensions, Text } from 'react-native';
+import { Animated, SafeAreaView, StatusBar, Dimensions, Text, View } from 'react-native';
 import { connect } from 'react-redux';
 import SearchComponent from '../../global/SearchComponent';
 import { createGetMethod } from '../../../api/Apis';
 import { endPoint, screens } from '../../../api/config';
 import LoaderComponent from '../../global/LoaderComponent';
-
+import ActionButton from 'react-native-action-button';
 export const deviceWidth = Dimensions.get('window').width;
 export const deviceHeight = Dimensions.get('window').height;
 
@@ -25,24 +25,24 @@ class QuanlyPhanQuyenScreen extends React.Component {
   }
 
   getNguoidung(datas) {
-      let url;
-      url = `${endPoint.getALLRoleName}?`;
-      url += `IsSearch=${encodeURIComponent(`${false}`)}&`;
-      url += `SkipCount=${encodeURIComponent(`${0}`)}&`;
-      url += `MaxResultCount=${encodeURIComponent(`${30}`)}`;
-      createGetMethod(url)
-        .then(res => {
-          if (res) {
-              console.log("Res:" +res.result.items);
-            this.setState({
-                toanboData: res.result.items,
-              total: res.result.totalCount
-            });
-          } else {
-            // Alert.alert('Lỗi khi load toàn bộ tài sản!');
-          }
-        })
-        .catch(err => console.log(err));
+    let url;
+    url = `${endPoint.getALLRoleName}?`;
+    url += `IsSearch=${encodeURIComponent(`${false}`)}&`;
+    url += `SkipCount=${encodeURIComponent(`${0}`)}&`;
+    url += `MaxResultCount=${encodeURIComponent(`${30}`)}`;
+    createGetMethod(url)
+      .then(res => {
+        if (res) {
+          console.log("Res:" + res.result.items);
+          this.setState({
+            toanboData: res.result.items,
+            total: res.result.totalCount
+          });
+        } else {
+          // Alert.alert('Lỗi khi load toàn bộ tài sản!');
+        }
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
@@ -64,45 +64,49 @@ class QuanlyPhanQuyenScreen extends React.Component {
       50,
     )
     return (
-      <Animated.View>
-        <StatusBar barStyle="dark-content" />
-        <SafeAreaView>
-          <SearchComponent
-            clampedScroll={clampedScroll}
-          />
-          <Animated.ScrollView
-            showsVerticalScrollIndicator={false}
+      <View style={{ flex: 1, backgroundColor: '#f3f3f3' }}>
+        <Animated.View>
+          <StatusBar barStyle="dark-content" />
+          <SafeAreaView>
+            <SearchComponent
+              clampedScroll={clampedScroll}
+            />
+            <Animated.ScrollView
+              showsVerticalScrollIndicator={false}
+              style={{
+                margin: 10,
+                paddingTop: 55,
+                paddingBottom: 15,
+              }}
+              contentContainerStyle={{
+                display: 'flex',
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+                justifyContent: 'space-around',
+                paddingBottom: 55,
+              }}
+              onScroll={Animated.event(
+                [{ nativeEvent: { contentOffset: { y: scrollYValue } } }],
+                { useNativeDriver: true },
+                () => { },          // Optional async listener
+              )}
+              contentInsetAdjustmentBehavior="automatic"
+            >
+              {LoaderComponent(toanboData, this.props, screens.quan_ly_phan_quyen)}
+            </Animated.ScrollView>
+          </SafeAreaView>
+          <Text
             style={{
-              margin: 10,
-              paddingTop: 55,
-              paddingBottom: 15,
+              bottom: 5,
+              right: 5,
+              position: 'absolute',
             }}
-            contentContainerStyle={{
-              display: 'flex',
-              flexDirection: 'row',
-              flexWrap: 'wrap',
-              justifyContent: 'space-around',
-              paddingBottom: 55,
-            }}
-            onScroll={Animated.event(
-              [{ nativeEvent: { contentOffset: { y: scrollYValue } } }],
-              { useNativeDriver: true },
-              () => { },          // Optional async listener
-            )}
-            contentInsetAdjustmentBehavior="automatic"
-          >
-            {LoaderComponent(toanboData, this.props, screens.quan_ly_phan_quyen)}
-          </Animated.ScrollView>
-        </SafeAreaView>
-        <Text
-          style={{
-          bottom: 5,
-          right: 5,
-          position: 'absolute',
-        }}
-        >Hiển thị: {toanboData.length}/{total}
-        </Text>
-      </Animated.View>
+          >Hiển thị: {toanboData.length}/{total}
+          </Text>
+        </Animated.View>
+        <ActionButton buttonColor="rgba(231,76,60,1)" position='right' onPress={() => this.props.navigation.navigate(screens.them_moi_vai_tro, { screen: "Thêm mới vai trò" })}></ActionButton>
+      </View>
+
     );
   }
 
